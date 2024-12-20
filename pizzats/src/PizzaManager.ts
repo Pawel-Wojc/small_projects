49d6d12e1a748a7b2372af7a151a51d563f9ff7a
Paweł Wójcik <62841636+Pawel-Wojc@users.noreply.github.com>
2024-12-20 11:23:48 +0100

    Update PizzaManager.ts

--- pizzats/src/PizzaManager.ts
+++ pizzats/src/PizzaManager.ts
@@ -5,42 +5,37 @@ import { PizzaDecorator } from "./PizzaDecorator";
 import { pizzaTypeChoices } from "./PizzaData";
 import { getExtraToppingChoices, getToppingChoicesForPizza } from "./Toppings";
 import { IPizza } from "./PizzaInterface";
+import { Choice } from "./Choice";
 
 export class PizzaManager {
+  menu: Choice<number>[] = [
+    { value: 1, name: "Package?", description: "asd" },
+    { value: 2, name: "Add another pizza" },
+    { value: 3, name: "Show Order" },
+    { value: 4, name: "Package?" },
+
+    { value: 5, name: "Create another order (clean current)" },
+    { value: 4, name: "Exit" },
+  ];
   view = new View();
   builder = new PizzaBuilder();
   async run() {
     let anotherPizza = true;
     const order: IPizza[] = [];
+    console.log("Welcome in pizza manager!");
     while (anotherPizza) {
-      await this.view.Confirm("Package?").then((res) => {
-        if (res) {
-          this.builder.addPackaging();
-        }
-      });
-      await this.view.Select("Size?", pizzaSizeChoices).then((res) => {
-        if (res !== null) {
-          this.builder.setSize(res!);
-        }
-      });
-
-      await this.view.Select("Select pizza?", pizzaTypeChoices).then((res) => {
-        this.builder.setType(res);
-      });
-
-      const toppings = getToppingChoicesForPizza(this.builder.getType()!);
-      if (toppings.length > 0) {
-        //if pizza has any toppings
-        await this.view.CheckBox("Delete toppings?", toppings).then((res) => {
-          this.builder.deleteToopings(res);
+      if (order.length == 0) {
+        await this.view.Select("Menu", [{ value: 1, name: "Add Pizza" }]).then((res) => {
+          if (res) {
+            console.log();
+            
+          }
         });
       }
 
-      while (await this.view.Confirm("Add any other toppings?")) {
-        const topping = await this.view.Select("Select topping to add", getExtraToppingChoices);
-        const quantity = await this.view.Number("Quantity");
-        this.builder.addToppings(topping, quantity);
-      }
+      await this.view.Select("Menu", this.menu).then((res) => {
+        console.log("menu selected" + res);
+      });
 
       let pizza = this.builder.build();
       if (pizza) {
@@ -54,6 +49,44 @@ export class PizzaManager {
           this.view.ShowOrder(order);
         }
       });
+    }
+  }
+
+  private async addPackage() {
+    await this.view.Confirm("Package?").then((res) => {
+      if (res) {
+        this.builder.addPackaging();
+      }
+    });
+  }
+
+  private async chooseSize() {
+    await this.view.Select("Size?", pizzaSizeChoices).then((res) => {
+      this.builder.setSize(res);
+    });
+  }
+
+  private async chooseType() {
+    await this.view.Select("Select pizza?", pizzaTypeChoices).then((res) => {
+      this.builder.setType(res);
+    });
+  }
+
+  private async deleteToppings() {
+    const toppings = getToppingChoicesForPizza(this.builder.getType()!);
+    if (toppings.length > 0) {
+      //if pizza has any toppings
+      await this.view.CheckBox("Delete toppings?", toppings).then((res) => {
+        this.builder.deleteToopings(res);
+      });
+    }
+  }
+
+  private async addAnotherToppings() {
+    while (await this.view.Confirm("Add any other toppings?")) {
+      const topping = await this.view.Select("Select topping to add", getExtraToppingChoices);
+      const quantity = await this.view.Number("Quantity");
+      this.builder.addToppings(topping, quantity);
     }
   }
 }
